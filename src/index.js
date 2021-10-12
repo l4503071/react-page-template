87ccfaca70473c782d72fdab6b496779fe923975
sunshine <sunliang3@xiaomi.com>
2021-10-12 19:22:48 +0800

    feat: normlize.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Layout from "./layout/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
+import "normalize.css";
 
 ReactDOM.render(
   <Provider store={store}>
