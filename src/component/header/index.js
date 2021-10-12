7343e2baee3faa0b4d630c13066c1ca74475ddeb
sunshine <sunliang3@xiaomi.com>
2021-10-12 19:04:15 +0800

    feat: 支持 scss

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-// import "./index.scss"
+import "./index.scss"
 
 export default function Header() {
   const menuList = useSelector((state) => state.common.menuList);
