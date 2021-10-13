cd1e950e71f3a74b604601eaa11c439adc213a77
sunshine <sunliang3@xiaomi.com>
2021-10-13 19:45:58 +0800

    feat: 自动加载 mock 文件

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,12 @@
 import Mock from "mockjs";
-import "./modules/menu";
-import "./modules/home";
+
+const context = require.context("./modules", false, /\.js$/);
+const mockFile = [];
+
+context.keys().forEach((key) => {
+  mockFile[key] = context(key);
+});
+console.log("%cmock 文件", "color: green", mockFile);
 
 Mock.setup({
   timeout: "500-1000",
