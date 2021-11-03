5375532ec71c7d43ee12eca02183e30133ab48e1
sunshine <sunliang3@xiaomi.com>
2021-11-03 19:33:10 +0800

    feat: sourceMap

--- src/page/sourcemap/index.js
+++ src/page/sourcemap/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import "./index.scss";
 import { Tabs, Button } from "antd";
 import Editor from "./component/Editor";
@@ -9,12 +10,13 @@ const { TabPane } = Tabs;
 
 function Sourcemap() {
   console.log("render sourcemap");
-  const [lineNo, setLineNo] = useState(458);
-  const [columnNo, setColumnNo] = useState(11);
-  const [url, setUrl] = useState("http://localhost:3000/static/js/8.chunk.js");
+  const [lineNo, setLineNo] = useState(-1);
+  const [columnNo, setColumnNo] = useState(-1);
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState("");
   useEffect(() => {
     window.onerror = function (msg, url, lineNo, columnNo) {
-      console.log("**", lineNo, columnNo, url);
+      console.log("1", lineNo, columnNo, url);
       setLineNo(lineNo);
       setColumnNo(columnNo);
       setUrl(url);
@@ -22,15 +24,26 @@ function Sourcemap() {
     };
   }, []);
   useEffect(async () => {
-    const mapContent =await getFileContentFromUrl(`${url}.map`);
+    console.log("2", lineNo, columnNo, url);
+    if (lineNo < 0 || columnNo < 0 || !!url) {
+      return;
+    }
+    const mapContent = await getFileContentFromUrl(`${url}.map`);
     sourceMap.SourceMapConsumer.with(mapContent, null, (consumer) => {
-      console.log("**", consumer.originalPositionFor({
-        line: lineNo,
-        column: columnNo,
-      }));
+      // console.log(consumer.originalPositionFor({
+      //   line: lineNo,
+      //   column: columnNo,
+      // }));
+      setResult(
+        JSON.stringify(
+          consumer.originalPositionFor({
+            line: lineNo,
+            column: columnNo,
+          })
+        )
+      );
     });
-    return () => {
-    };
+    return () => {};
   }, [lineNo, columnNo, url]);
   function onClickTest_1() {
     throw new Error("test 1...");
@@ -43,14 +56,19 @@ function Sourcemap() {
       </div>
       <Tabs defaultActiveKey="1" className="sourcemap__test">
         <TabPane tab="测试用例 1" key="1">
-          <Button type="primary" className="sourcemap__test__btn" onClick={onClickTest_1}>测试</Button>
+          <div>
+            <Button type="primary" className="sourcemap__test__btn" onClick={onClickTest_1}>
+              测试
+            </Button>
+            {!!result && result}
+          </div>
           <Editor content={readFileContent("page")} className="sourcemap__test__editor" startLine={10} endLine={16} />
         </TabPane>
         <TabPane tab="测试用例 2" key="2">
-            Content of Tab Pane 2
+          Content of Tab Pane 2
         </TabPane>
         <TabPane tab="测试用例 3" key="3">
-            Content of Tab Pane 3
+          Content of Tab Pane 3
         </TabPane>
       </Tabs>
     </div>
